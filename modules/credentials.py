4299c931704ff18078bbca2888ff9273867600c3
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2021-01-07 21:34:00 -0800

    Update and rename credentials.py.example to credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 Atul-Kadian
 
 class Creds(object):
-        TOKEN = "69696969:YoUrBoTtOkEnFrOmBoTfAtHeR"
+        TOKEN = "1550308808:AAEARn87Q3IVU_1ZNBtVilFnzxvDt5HiSNc"
         CLIENT_ID = "6969696969-gEtItFrOmGoOgLeClOuDcOnSoLe.apps.googleusercontent.com"
         CLIENT_SECRET = "gEtItFrOmGoOgLeClOuDcOnSoLe"
 
